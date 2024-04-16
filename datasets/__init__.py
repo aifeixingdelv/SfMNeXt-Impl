a5b2245ee99685be6cf84f22268ce67b65e6f6bb
aifeixingdelv <67185045+aifeixingdelv@users.noreply.github.com>
2024-04-16 13:36:49 +0800

    update

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,3 +6,4 @@ from .mono_dataset import *
 from .mono_dataset_city import *
 from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
 from .cityscapes_evaldataset import CityscapesEvalDataset
+from .automine_dataset  import AutoMineDepthDataset
